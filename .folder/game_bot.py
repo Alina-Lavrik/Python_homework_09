61e135cadf2f6583a8a3b8ff603932b7fcbc9424
Alina Lavrik <alina_voro@mail.ru>
2023-01-03 14:34:42 +0300

    Add game with bot_2

--- .folder/game_bot.py
+++ .folder/game_bot.py
@@ -7,21 +7,21 @@ from random import randint
 from random import choice
 
 bot = telebot.TeleBot("5924736461:AAHyjUG9LH3WdWrY9SIPRAxScaXYx4GsqQE")
-candys = dict()
-enable_game = dict()
-turn = dict()
+sweets = dict()
+start_game = dict()
+player = dict()
 
 
 def handle_game_proc(message):
-    global enable_game
+    global start_game
     try:
-        if enable_game[message.chat.id] and 1 <= int(message.text) <= 28:
+        if start_game[message.chat.id] and 1 <= int(message.text) <= 28:
             return True
         else:
             return False
     except KeyError:
-        enable_game[message.chat.id] = False
-        if enable_game[message.chat.id] and 1 <= int(message.text) <= 28:
+        start_game[message.chat.id] = False
+        if start_game[message.chat.id] and 1 <= int(message.text) <= 28:
             return True
         else:
             return False
@@ -29,45 +29,45 @@ def handle_game_proc(message):
 
 @bot.message_handler(commands=['game'])
 def send_welcome(message):
-    global turn, candys, enable_game
+    global player, sweets, start_game
     bot.reply_to(message, "Let's go")
-    candys[message.chat.id] = 117
-    turn[message.chat.id] = choice(['Bot', 'User'])
-    bot.send_message(message.chat.id, f'Начинает {turn[message.chat.id]}')
-    enable_game[message.chat.id] = True
-    if turn[message.chat.id] == 'Bot':
-        take = randint(1, candys[message.chat.id] % 28)
-        candys[message.chat.id] -= take
+    sweets[message.chat.id] = 117
+    player[message.chat.id] = choice(['Bot', 'User'])
+    bot.send_message(message.chat.id, f'Начинает {player[message.chat.id]}')
+    start_game[message.chat.id] = True
+    if player[message.chat.id] == 'Bot':
+        take = randint(1, sweets[message.chat.id] % 28)
+        sweets[message.chat.id] -= take
         bot.send_message(message.chat.id, f'Бот взял {take}')
         bot.send_message(message.chat.id,
-                         f'Осталось {candys[message.chat.id]}')
-        turn[message.chat.id] = 'User'
+                         f'Осталось {sweets[message.chat.id]}')
+        player[message.chat.id] = 'User'
 
 
 @bot.message_handler(func=handle_game_proc)
 def game_process(message):
-    global candys, turn, enable_game
-    if turn[message.chat.id] == 'User':
-        if candys[message.chat.id] > 28:
-            candys[message.chat.id] -= int(message.text)
+    global sweets, player, start_game
+    if player[message.chat.id] == 'User':
+        if sweets[message.chat.id] > 28:
+            sweets[message.chat.id] -= int(message.text)
             bot.send_message(message.chat.id,
-                             f'Осталось {candys[message.chat.id]}')
-            if candys[message.chat.id] > 28:
-                take = randint(1, candys[message.chat.id] % 28)
-                candys[message.chat.id] -= take
+                             f'Осталось {sweets[message.chat.id]}')
+            if sweets[message.chat.id] > 28:
+                take = randint(1, sweets[message.chat.id] % 28)
+                sweets[message.chat.id] -= take
                 bot.send_message(message.chat.id,
                                  f'Бот взял {take}')
                 bot.send_message(message.chat.id,
-                                 f'Осталось {candys[message.chat.id]}')
-                if candys[message.chat.id] <= 28:
+                                 f'Осталось {sweets[message.chat.id]}')
+                if sweets[message.chat.id] <= 28:
                     bot.send_message(message.chat.id, 'User Win')
-                    enable_game[message.chat.id] = False
+                    start_game[message.chat.id] = False
             else:
                 bot.send_message(message.chat.id, 'Bot Win')
-                enable_game[message.chat.id] = False
+                start_game[message.chat.id] = False
         else:
             bot.send_message(message.chat.id, 'Bot Win')
-            enable_game[message.chat.id] = False
+            start_game[message.chat.id] = False
 
 
 bot.infinity_polling()
